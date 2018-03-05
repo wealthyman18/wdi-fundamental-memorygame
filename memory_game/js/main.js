9762db854d6ba9f1ba58d7dbd4778058c8184db0
wealthyman18 <36753634+wealthyman18@users.noreply.github.com>
2018-03-05 10:47:54 -0500

    Add files via upload

---
+++ memory_game/js/main.js
@@ -0,0 +1,24 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+var cards = ["queens","queens","kings","kings"];
+var cardsInPlay = [];
+var cardOne = ["queens"];
+    cardsInPlay . push(cardOne);
+console.log("User flipped queen");
+var cardTwo = ["kings"];
+    cardsInPlay . push(cardTwo);
+console.log("User flipped kings");
+if (x = = = 2) {
+	console . log("cardsInPlayOne");
+} else if {
+    console . log("cardsInplayTwo");
+} else {
+alert("You have a match");
+}
+
+  
